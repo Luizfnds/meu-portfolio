c2285e05460187cb530be372070824e119b0c504
Luizfnds <luizeduardofnds@gmail.com>
2022-07-03 00:24:41 -0300

    Melhorando estrutura

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+/*---------- ScrollReveal ----------*/ 
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".conteudo-principal-apresentacao-um-texto", { duration: 1000 });
+sr.reveal(".conteudo-principal-apresentacao-um-logos", { duration: 1000 });
+
+sr.reveal(".conteudo-principal-apresentacao-dois-retrato", { duration: 1000 });
+sr.reveal(".conteudo-principal-apresentacao-dois-titulo", { duration: 1000 });
+sr.reveal(".conteudo-principal-apresentacao-dois-texto", { duration: 1000 });
+

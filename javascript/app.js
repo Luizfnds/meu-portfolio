75da24cf9e131a3678165f53a746ad4edf5ac7dc
Luizfnds <luizeduardofnds@gmail.com>
2022-07-07 18:58:42 -0300

    Refatorando ainda mais primeira e segunda seção

--- javascript/app.js
+++ javascript/app.js
@@ -2,14 +2,12 @@
 
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal(".conteudo-principal-apresentacao-um-texto", { duration: 1000 });
-sr.reveal(".conteudo-principal-apresentacao-um-logos", { duration: 1000 });
+sr.reveal(".secao-um-container-um-texto", { duration: 1000 });
+sr.reveal(".secao-um-container-um-links", { duration: 1000 });
 
-sr.reveal(".conteudo-principal-apresentacao-dois-retrato", { duration: 1000 });
-sr.reveal(".conteudo-principal-apresentacao-dois-titulo", { duration: 1000 });
-sr.reveal(".conteudo-principal-apresentacao-dois-texto", { duration: 1000 });
-sr.reveal(".conteudo-secundario-projetos", { duration: 1000 });
+sr.reveal(".secao-um-container-dois-foto", { duration: 1000 });
+sr.reveal(".secao-um-container-dois-titulo", { duration: 1000 });
+sr.reveal(".secao-um-container-dois-texto", { duration: 1000 });
+sr.reveal(".secao-dois-projetos", { duration: 1000 });
 
-
-sr.reveal(".imagem-teste", { duration: 1000 });
 
